4789326f2a9958d5110623c9bf8e0c5ff5e530c0
CamilaVelezGiraldo <111664816+velitaz@users.noreply.github.com>
2025-05-01 11:54:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 import base64
 
 st.title("Conversión de Texto a Audio")
-image = Image.open('gato_raton.png')
+image = Image.open('zorro_uvas.png')
 st.image(image, width=350)
 with st.sidebar:
     st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
